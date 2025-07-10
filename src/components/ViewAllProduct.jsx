3402ac02f3d76e37feb564f0c66bc94ff450926c
merin20git <merin20122003@gmail.com>
2025-07-10 22:55:14 +0530

    first commit

---
+++ src/components/ViewAllProduct.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+
+const ViewAllProduct = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OLAY</h5>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OLAY</h5>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OLAY</h5>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OLAY</h5>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OLAY</h5>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OLAY</h5>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OLAY</h5>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OLAY</h5>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">OLAY</h5>
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://th.bing.com/th?q=Facial+Cream+Product&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.5&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title"> OLAY</h5>
+                    
+                                        <a href="#" class="btn btn-primary">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAllProduct
