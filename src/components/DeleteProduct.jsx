90cd2bf74375ab7fa5dfd3a2bcf7115724b0eb3f
merin20git <merin20122003@gmail.com>
2025-07-11 12:09:39 +0530

    second commit

--- src/components/DeleteProduct.jsx
+++ src/components/DeleteProduct.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import ProductNavbar from './ProductNavbar'
 
 const DeleteProduct = () => {
   return (
     <div>
+      <ProductNavbar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -12,7 +14,7 @@ const DeleteProduct = () => {
                             <input type="text" className="form-control" />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-success">Delete</button>
+                            <button className="btn btn-danger">Delete</button>
                         </div>
                     </div>
                 </div>
